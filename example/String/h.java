6347b19e6b679f6e3e21d1430082f25e29b8b880
Yashaswini Mudragadda <mudragaddarajaniyashaswini@gmail.com>
2026-03-03 14:22:05 +0530

    Create h.java

---
+++ example/String/h.java
@@ -0,0 +1,17 @@
+import java.util.StringTokenizer;
+public class Test56
+{
+	public static void main(String args[])
+	{
+String fruits[]=new String[5];
+fruits[0]="Apple";
+fruits[1]="Mango";
+fruits[2]="Orange";
+fruits[3]="Banana";
+fruits[4]="Grapes";
+for(String f:fruits)
+{
+	System.out.print("\t"+f);
+}
+}
+}
